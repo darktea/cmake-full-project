6831e4ca29f709b74e03695bb3a2bc77e2c3f191
darktea <tiantianding@gmail.com>
2022-05-16 08:40:31 +0800

    more comments on cmake

--- cmake/GetFTXUI.cmake
+++ cmake/GetFTXUI.cmake
@@ -1,11 +1,20 @@
+# 当前在使用 CMake（3.14 版本之后）时，推荐使用 FetchContent 模块来引入外部项目
+# FetchContent 是对原先的 ExternalProject 模块更好的封装
+
+# Step1：使用 include 命令加载 FetchContent 模块
 include(FetchContent)
 
+# Step2：使用 FetchContent_Declare 配置要引入的依赖
 FetchContent_Declare(
   FTXTUI
   GIT_REPOSITORY https://github.com/ArthurSonzogni/FTXUI.git
   GIT_TAG        v0.11
 )
+
+# 本例子中，还需要使用 option 命令关闭以下选项
 option(FTXUI_ENABLE_INSTALL "" OFF)
 option(FTXUI_BUILD_EXAMPLES "" OFF)
 option(FTXUI_BUILD_DOCS "" OFF)
+
+# Step3：使用 FetchContent_MakeAvailable 进行依赖的下载，构建，安装等工作
 FetchContent_MakeAvailable(FTXTUI)
--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -1,6 +1,11 @@
+# 在当前目录，及其子目录开启 CMake 的 CTest 功能
 enable_testing()
 
+# 准备获取外部依赖 googletest（使用 FetchContent 模块）
 include(FetchContent)
+
+# 获取外部依赖分 2 步。
+# 第一步 Declare，并设置 option
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
@@ -11,12 +16,15 @@ FetchContent_Declare(
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 option(INSTALL_GMOCK "Install GMock" OFF)
 option(INSTALL_GTEST "Install GTest" OFF)
+
+# 第二步 获取、编译等工作
 FetchContent_MakeAvailable(googletest)
 
 include(GoogleTest)
 include(Coverage)
 include(Memcheck)
 
+# 把可复用的部分用 macro 封装成一个 AddTest
 macro(AddTests target)
   AddCoverage(${target})
   target_link_libraries(${target} PRIVATE gtest_main gmock)
